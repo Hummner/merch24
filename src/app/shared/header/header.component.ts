aa4124a10f0c6e7a1eb1f9304b3f32f6347c9f35
Hummner <bence.cservenyak@gmail.com>
2025-08-19 20:25:40 +0200

    add: header opacity

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,17 +3,46 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink } from "@angular/router";
+import { ViewportScroller } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatMenuModule, RouterLink],
+  imports: [MatIconModule, MatButtonModule, MatMenuModule, RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  viewPosition!: [number, number];
+  headerOpacity = true;
 
+  constructor(private view: ViewportScroller) {
+    this.viewPosition = view.getScrollPosition()
 
   }
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll() {
+    this.viewPosition = this.view.getScrollPosition();
+    
+    let sum = this.viewPosition.reduce((a, b) => a + b, 0)
+    if (sum == 0) {
+      this.headerOpacity = true;
+    } else {
+      this.headerOpacity = false
+    }
+
+    console.log(this.headerOpacity);
+    
+    
+  } 
+
+  getOpacity() {
+    return this.headerOpacity ? 'opacity' : '-'
+  }
+
+
+}
