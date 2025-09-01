3ab730112f3699d710ddb2deb76b177f3d7702a4
Hummner <bence.cservenyak@gmail.com>
2025-09-01 07:34:18 +0200

    add: privacy-policy and add: more services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { ProductComponent } from './product/product.component';
 import { ContactComponent } from './contact/contact.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     {path: "", component: LandingPageComponent},
     {path: "impressum", component: ImpressumComponent},
     { path: "product/:id", component: ProductComponent },
-    { path: "contact", component: ContactComponent}
+    { path: "contact", component: ContactComponent},
+    { path: "privacy-policy", component: PrivacyPolicyComponent}
+    
 ];
--- src/app/services/category-service.service.ts
+++ src/app/services/category-service.service.ts
@@ -9,6 +9,31 @@ export class CategoryServiceService {
 
   categories = [
     {
+      title: "Stickerei – edel & langlebig",
+      text: "Ob Firmenlogo, Vereinswappen oder Namenszug – unsere Stickerei verleiht Textilien eine hochwertige, langlebige Veredelung.",
+      img: "sticker",
+    },
+    {
+      title: "Flexdruck & Foliendruck & DTF & UV-DTF & Siebdruck",
+      text: "Flexdruck mit kräftigen Farben, Foliendruck mit Effekten, detailreiches DTF, robustes UV-DTF und langlebiger Siebdruck für große Stückzahlen.",
+      img: "flex"
+    },
+    {
+      title: "Grafikdesign – dein Look, dein Stil",
+      text: "Von Logos über Flyer bis hin zu kompletten Branding-Konzepten – wir gestalten visuelle Designs, die auffallen und im Gedächtnis bleiben.",
+      img: "design",
+    },
+    {
+      title: "Fotogeschenke & Bands",
+      text: "Ob personalisierte Fotogeschenke für besondere Anlässe oder individuell veredelte Textilien für Musikbands – wir sorgen für kreative und langlebige Ergebnisse.",
+      img: "photogift",
+    },
+    {
+      title: "Professionelle Fotografie – für besondere Momente",
+      text: "Ob Portrait, Event oder Produktfoto – wir setzen dich und deine Ideen perfekt in Szene und halten deine schönsten Augenblicke fest.",
+      img: "photos",
+    },
+    {
       title: "Individuelle Pokale & Medaillen – nach Wunsch gestaltet",
       text: "Ob Sportturnier, Firmenfeier, Kindergeburtstag oder Schulfest – bei uns bekommst du Pokale und Medaillen, die perfekt zum Anlass passen.",
       img: "cups",
@@ -20,25 +45,13 @@ export class CategoryServiceService {
       text: "Von T-Shirts über Hoodies bis hin zu Taschen – wir bedrucken deine Textilien mit hochwertigen Motiven, Logos oder Sprüchen ganz nach deinen Vorstellungen.",
       img: "shirt",
     },
-    {
-      title: "Professionelle Fotografie – für besondere Momente",
-      text: "Ob Portrait, Event oder Produktfoto – wir setzen dich und deine Ideen perfekt in Szene und halten deine schönsten Augenblicke fest.",
-      img: "photos",
-    },
-    {
-      title: "Grafikdesign – dein Look, dein Stil",
-      text: "Von Logos über Flyer bis hin zu kompletten Branding-Konzepten – wir gestalten visuelle Designs, die auffallen und im Gedächtnis bleiben.",
-      img: "design",
-    },
+
+
     {
       title: "Teamwear von Jako & Puma – einheitlich auftreten",
       text: "Statte dein Team mit hochwertiger Kleidung von Marken wie Jako oder Puma aus – individuell bedruckt und perfekt abgestimmt.",
       img: "jersey",
     },
-    {
-      title: "Stickerei – edel & langlebig",
-      text: "Ob Firmenlogo, Vereinswappen oder Namenszug – unsere Stickerei verleiht Textilien eine hochwertige, langlebige Veredelung.",
-      img: "sticker",
-    },
+
   ]
 }
