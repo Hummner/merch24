498f84f39cff20c5d6b91eb4bad9a1a3c4e4c5ee
Hummner <bence.cservenyak@gmail.com>
2026-03-25 13:42:25 +0100

    work on stepper

--- src/app/order/shippingcart-detail/shippingcart-detail.component.ts
+++ src/app/order/shippingcart-detail/shippingcart-detail.component.ts
@@ -3,18 +3,22 @@ import { ShippingcartServiceService } from '../../services/shippingcart-service.
 import { Article } from '../../interfaces/article';
 import { CartItems } from '../../interfaces/cart-items';
 import { MatStepper, MatStep, MatStepLabel } from "@angular/material/stepper";
+import { CommonModule } from '@angular/common';
 
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 
 @Component({
   selector: 'app-shippingcart-detail',
   standalone: true,
-  imports: [MatStepper, MatStep, MatStepLabel,MatStepperModule, MatButtonModule],
+  imports: [MatStepper, MatStep, MatStepLabel, MatStepperModule, MatButtonModule, MatFormFieldModule, ReactiveFormsModule, MatInputModule, MatIconModule, CommonModule],
   templateUrl: './shippingcart-detail.component.html',
   styleUrl: './shippingcart-detail.component.scss'
 })
@@ -26,6 +30,22 @@ export class ShippingcartDetailComponent implements OnInit {
   shippingCost!: number;
   totalPrice!: number;
   private shippingcartService = inject(ShippingcartServiceService);
+  private _formBuilder = inject(FormBuilder);
+  showDifferentBillingAddress  = false;
+
+
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'create',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/create.svg')
+    );
+  }
+
+
 
   ngOnInit(): void {
     this.shippingcart.cart$.subscribe((items) => {
@@ -36,6 +56,26 @@ export class ShippingcartDetailComponent implements OnInit {
 
     })
   }
+
+
+
+  firstFormGroup = this._formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    address: ['', Validators.required],
+    zip: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]],
+    city: ['', Validators.required],
+  });
+
+  secondFormGroup = this._formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    address: ['', Validators.required],
+    zip: ['', [Validators.required, Validators.pattern(/^\d{5}$/)]],
+    city: ['', Validators.required],
+  });
 
   deleteItemFromCart(item: CartItems) {
     this.shippingcartService.delelteItem(item);
