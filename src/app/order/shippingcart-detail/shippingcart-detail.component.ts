8a632e80fdd11b6caaaf6b1edd9154b7433b590c
Hummner <bence.cservenyak@gmail.com>
2026-03-25 07:34:52 +0100

    add stepper into Order site

--- src/app/order/shippingcart-detail/shippingcart-detail.component.ts
+++ src/app/order/shippingcart-detail/shippingcart-detail.component.ts
@@ -2,11 +2,19 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ShippingcartServiceService } from '../../services/shippingcart-service.service';
 import { Article } from '../../interfaces/article';
 import { CartItems } from '../../interfaces/cart-items';
+import { MatStepper, MatStep, MatStepLabel } from "@angular/material/stepper";
+
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-shippingcart-detail',
   standalone: true,
-  imports: [],
+  imports: [MatStepper, MatStep, MatStepLabel,MatStepperModule, MatButtonModule],
   templateUrl: './shippingcart-detail.component.html',
   styleUrl: './shippingcart-detail.component.scss'
 })
