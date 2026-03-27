fb0079764efb8d4436820b18456ff95430f7a54a
Hummner <bence.cservenyak@gmail.com>
2026-03-27 07:47:28 +0100

    add validator to formGroups

--- src/app/order/shippingcart-detail/shippingcart-detail.component.ts
+++ src/app/order/shippingcart-detail/shippingcart-detail.component.ts
@@ -12,7 +12,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormBuilder, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import {MatRadioModule} from '@angular/material/radio';
+import { MatRadioModule } from '@angular/material/radio';
 
 
 
@@ -28,16 +28,12 @@ export class ShippingcartDetailComponent implements OnInit {
   shippingcart = inject(ShippingcartServiceService);
   cart!: CartItems[];
   sumPrice!: number;
-  shippingCost!: number;
-  totalPrice!: number;
   private shippingcartService = inject(ShippingcartServiceService);
   private _formBuilder = inject(FormBuilder);
-  showDifferentBillingAddress  = false;
+  showDifferentBillingAddress = false;
 
   shipments: string[] = ['Lieferung', 'Abholung'];
   payments: string[] = ['Überweisung', 'Barzahlung'];
-  
-
 
 
   constructor(
@@ -50,19 +46,23 @@ export class ShippingcartDetailComponent implements OnInit {
     );
   }
 
-
-
   ngOnInit(): void {
     this.shippingcart.cart$.subscribe((items) => {
       this.cart = items;
       this.sumPrice = this.shippingcart.totalPrice;
-      this.shippingCost = this.shippingcart.shippingCost;
-      this.totalPrice = this.shippingCost + this.sumPrice;
-
     })
   }
 
+  get shippingCost(): number {
+    if (this.shipmentControl.value != "" && this.shipmentControl.value == this.shipments[this.shipments.indexOf("Lieferung")]) {
+      return this.shippingcart.shippingCost
+    }
+    return 0
+  }
 
+  get totalPrice(): number {
+    return this.shippingCost + this.sumPrice;
+  }
 
   firstFormGroup = this._formBuilder.group({
     firstName: ['', Validators.required],
@@ -87,6 +87,40 @@ export class ShippingcartDetailComponent implements OnInit {
 
   deleteItemFromCart(item: CartItems) {
     this.shippingcartService.delelteItem(item);
+  }
+
+  toOverview() {
+    console.log(this.firstFormGroup.status,
+    this.paymentControl.status,
+    this.shipmentControl.status,
+  this.secondFormGroup.status);
+
+    if (!this.checkForms()) return
+
+    alert("Danke für deine Bestellung")
+
+  }
+
+  checkForms() {
+    this.firstFormGroup.markAllAsTouched();
+    this.paymentControl.markAllAsTouched();
+    this.shipmentControl.markAllAsTouched();
+
+    if (this.showDifferentBillingAddress) {
+      this.secondFormGroup.markAllAsTouched();
+      if (this.checkFormsWithBillingAddress()) return true
+      return false
+    }
+    if (this.checkFormsWithoutBillingAddress()) return true
+    return false
+  }
+
+  checkFormsWithoutBillingAddress() {
+    return this.firstFormGroup.valid && this.paymentControl.valid && this.shipmentControl.valid
+  }
+
+  checkFormsWithBillingAddress() {
+    return this.firstFormGroup.valid && this.paymentControl.valid && this.shipmentControl.valid && this.secondFormGroup.valid
   }
 
 }
